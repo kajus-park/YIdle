f02b91f09af22312ed62ec6746b863a7770a1b6a
Jan Schulz <jpmaster84@gmail.com>
2023-03-22 21:54:42 +0100

    Refactored imports and hid console

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use eframe::egui;
-use eframe::App;
-use eframe::NativeOptions;
-use eframe::glow::Context;
+#![windows_subsystem = "windows"]
+use eframe::{egui, App, NativeOptions};
 use egui::CentralPanel;
 use jk_ban::BigNum;
+
 
 fn main() -> Result<(), eframe::Error> {
     let options = NativeOptions::default();
@@ -23,14 +22,8 @@ enum Screen {
 struct IdleGame {
     clicks: BigNum,
     open_screen: Screen,
-    clicker1: BigNum,
+    clicker1: BigNum,  
     update_thread: Option<std::thread::JoinHandle<()>>,
-}
-
-impl IdleGame {
-    fn test(&mut self, ctx: egui::Context){
-        self.update_thread = Some(std::thread::spawn(move || loop{ctx.request_repaint();}));
-    }
 }
 
 impl App for IdleGame {
@@ -43,6 +36,7 @@ impl App for IdleGame {
                         ui.separator();
                         ui.label(format!("Clicks: {}", self.clicks));
                         ui.label(format!("Clickers: {}", self.clicker1));
+                        ui.separator();
                         if ui.button("Click").clicked() {
                             self.clicks.increment();
                         }
@@ -50,16 +44,22 @@ impl App for IdleGame {
                             self.clicker1.increment();
                         }
                         if ui.button("Square").clicked() {
-                            self.clicks=self.clicks.pow(2.0);
+                            self.clicks = self.clicks.pow(2.0);
+                        }
+                        if ui.button("Square Clicker").clicked() {
+                            self.clicker1 = self.clicker1.pow(2.0);
                         }
                     });
                 });
             }
         }
 
-        self.clicks= self.clicks+self.clicker1;
+        self.clicks = self.clicks + self.clicker1;
         let ctx_clone = ctx.clone();
-        if let None = self.update_thread {self.update_thread=
-            Some(std::thread::spawn(move || loop{ctx_clone.request_repaint();}))}
+        if let None = self.update_thread {
+            self.update_thread = Some(std::thread::spawn(move || loop {
+                ctx_clone.request_repaint();
+            }))
+        }
     }
 }
